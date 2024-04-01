b9329f1a1b15181d4feff4da0ae9adc10ccb98f8
Gunnar Moody <moodygman04@gmail.com>
2024-04-01 12:19:52 -0500

    Fixed duplicate string in models.py

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -66,7 +66,6 @@ class BookInstance(models.Model):
     
 class Author(models.Model):
     """Model representing an author."""
-    """Model representing an author."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
