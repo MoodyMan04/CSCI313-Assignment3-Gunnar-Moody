cf1356f31eaa344ec9365e7254670b8375a8c980
Gunnar Moody <moodygman04@gmail.com>
2024-04-01 21:40:35 -0500

    added num_genres + num_books_road to view and index.html

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -14,11 +14,19 @@ def index(request):
     # Generate count of Authors.
     num_authors = Author.objects.count()
 
+    # Generate count of Genres.
+    num_genres = Genre.objects.all().count()
+
+    # Generate count of Books with word 'Road' in the title
+    num_books_road = Book.objects.all().filter(title__contains='road').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_road': num_books_road
     }
 
     return render(request, 'index.html', context=context)
