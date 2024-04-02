d2defbacbc300d80ee77b9c8c5b526a52e2aeb22
Gunnar Moody <moodygman04@gmail.com>
2024-04-02 16:52:21 -0500

    added accounts, logging in/out, borrowed books, etc.

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
+from django.conf import settings
+from datetime import date
 import uuid
 
 class Genre(models.Model):
@@ -52,6 +54,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -65,7 +68,14 @@ class BookInstance(models.Model):
     class Meta:
         """Class for metadata of the BookInstance model."""
         ordering = ['due_back']
+        permissions = (('can_mark_returned', 'Set book as returned'), )
     
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
+
     def __str__(self) -> str:
         """Returns a string representation of a BookInstance."""
         return f'{self.id} ({self.book.title})'
